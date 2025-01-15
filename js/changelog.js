85d1211d893adc5be1d044f496340195ddd8dd13
Bob <extractwithbob@gmail.com>
2025-01-14 23:17:27 -0300

    Added All Class Voices

--- js/changelog.js
+++ js/changelog.js
@@ -6,16 +6,21 @@ document.addEventListener("DOMContentLoaded", () => {
             version: "v1.0",
             date: "2024-12-24",
             changes: [
-                "Launch of Bob Comparer"
-				"v255 -- The Dark Ride available"
+                "Launch of Bob Comparer",
+				"v255 -- The Dark Ride available",
 				"v256 -- The Dark Ride -- Limbo available"
             ]
         },
+		
         {
-            version: "",
-            date: "",
+            version: "v1.1",
+            date: "2025-01-14",
             changes: [
-                ""
+                'Added the "All Class Voices" page.',
+                "Added Achievements, a preliminary version that still needs more work.",
+                "Modified the home page to include a brief explanation of the project's purpose.",
+                'Fixed the "Change Log" links for all files.',
+                "Fixed the CSS for the logo."
             ]
         },
     ];
